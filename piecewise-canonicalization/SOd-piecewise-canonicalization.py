e3e71e9ab99ee74254843fcd5fea3f714c984916
Nathaniel Kenschaft <nkenschaft@gmail.com>
2026-06-24 17:07:44 -0400

    EOD 6/24

--- piecewise-canonicalization/SOd-piecewise-canonicalization.py
+++ piecewise-canonicalization/SOd-piecewise-canonicalization.py
@@ -3,6 +3,13 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader, TensorDataset
 
+import matplotlib
+matplotlib.use('TkAgg')
+from matplotlib import pyplot as plt
+from matplotlib.ticker import MaxNLocator, AutoMinorLocator
+
+import pickle
+
 
 def sym(A : torch.Tensor) -> torch.Tensor:
     return A.mT @ A
@@ -26,9 +33,20 @@ def canonicalize(A : torch.Tensor) -> torch.Tensor:
     return T.unsqueeze(-2) * psd_matrix_sqrt(symA)
 
 
-def generate_dataset(num_samples : int, d : int) -> tuple[torch.Tensor, torch.Tensor]:
+def quotient(A : torch.Tensor) -> torch.Tensor:
+    assert A.shape[-1] == A.shape[-2], "quotient is currently only defined for square matrices"
+    sign, _ = torch.linalg.slogdet(A)
+    return sign[:, None, None] * sym(A)
+
+
+def martrix_polynomial(A : torch.Tensor, coefficients : tuple[float,...]):
+    return sum(c * torch.linalg.matrix_power(A,n) for n, c in enumerate(coefficients))
+
+
+def generate_dataset(num_samples : int, d : int, polynomial : tuple[float,...]) -> tuple[torch.Tensor, torch.Tensor]:
     X = torch.randn(num_samples, d, d)
-    y = torch.linalg.matrix_power(sym(X), 2).diagonal(dim1=-2, dim2=-1).sum(dim=-1)
+    # y = torch.linalg.matrix_power(sym(X), 3).diagonal(dim1=-2, dim2=-1).sum(dim=-1)
+    y = martrix_polynomial(sym(X), polynomial).diagonal(dim1=-2, dim2=-1).sum(dim=-1)
     return X, y
 
 
@@ -49,39 +67,46 @@ class MLP(nn.Module):
         return self.mlp(x_flat).squeeze()
 
 
-def get_random_rotation(d : int) -> torch.Tensor:
-    A = torch.randn(d, d)
+def get_random_rotations(batch_size : int, d : int) -> torch.Tensor:
+    A = torch.randn(batch_size, d, d)
     Q, R = torch.linalg.qr(A)
     d_sign = torch.det(Q).sign()
-    Q[:, 0] *= d_sign
+    Q[:,:,-1] *= d_sign[:, None] # multiply sign on last column to force determinant to be 1
     return Q
 
 
-if __name__ == "__main__":
-    # torch.manual_seed(42)
-    d = 4 
-    epochs = 100
-    batch_size = 64
-    
+def train_test(d=4, num_epochs=1000, batch_size=128, mode="none", mlp_hidden_dims=None, polynomial=(0,2)):
+    if mlp_hidden_dims is None: mlp_hidden_dims=(d**2,)
     # generate data
-    X_train, y_train = generate_dataset(num_samples=8000, d=d)
-    # X_train = canonicalize(X_train)
-    # X_train = sym(X_train)
-    X_test, y_test = generate_dataset(num_samples=1000, d=d)
-    # X_test = canonicalize(X_test)
+    X_train, y_train = generate_dataset(num_samples=8000, d=d, polynomial=polynomial)
+    X_test, y_test = generate_dataset(num_samples=1000, d=d, polynomial=polynomial)
+    R = get_random_rotations(len(X_test), d)
+    X_test_rotated = R @ X_test
+    if mode == "canonicalize":
+        X_train = canonicalize(X_train)
+        X_test = canonicalize(X_test)
+        X_test_rotated = canonicalize(X_test_rotated)
+    elif mode == "quotient":
+        X_train = quotient(X_train)
+        X_test = quotient(X_test)
+        X_test_rotated = quotient(X_test_rotated)
+
     # X_test = sym(X_test)
     dataset = TensorDataset(X_train, y_train)
     loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
     
     # initialize model
-    model = MLP(d=d, hidden_dims=(32,))
+    model = MLP(d=d, hidden_dims=mlp_hidden_dims)
     criterion = nn.MSELoss()
     optimizer = optim.Adam(model.parameters(), lr=0.005)
     
-    # train
-    model.train()
+    train_losses = list()
+    test_losses = list()
+    rotated_test_losses = list()
     print("Training...")
-    for epoch in range(epochs):
+    for epoch in range(num_epochs):
+        # train
+        model.train()
         epoch_loss = 0
         for batch_x, batch_y in loader:
             optimizer.zero_grad()
@@ -90,28 +115,128 @@ if __name__ == "__main__":
             loss.backward()
             optimizer.step()
             epoch_loss += loss.item() * batch_x.size(0)
+        train_mse = epoch_loss / len(X_train)
         
-        print(f"Epoch {epoch+1}/{epochs} | Training MSE Loss: {epoch_loss / len(X_train):.4f}")
+        # test
+        model.eval()
+        with torch.no_grad():
+            test_preds = model(X_test)
+            test_mse = criterion(test_preds, y_test).item()
+            rotated_preds = model(X_test_rotated)
+            rotated_test_mse = criterion(rotated_preds, y_test).item()
+        
+        train_losses.append(train_mse)
+        test_losses.append(test_mse)
+        rotated_test_losses.append(rotated_test_mse)
+        print(f"Epoch {epoch+1:03d}/{num_epochs}")
+        print("==============================")
+        print(f"Train Loss: {train_mse:.4f}")
+        print(f"Test Loss: {test_mse:.4f}")
+        print(f"Rotated Test Loss: {rotated_test_mse:.4f}")
+        print("\033[5A", end="")
+    print(end="\n\n\n\n\n")
+    print("Done!")
+    print()
+    
+    return train_losses, test_losses, rotated_test_losses
 
-    # Evaluate
-    model.eval()
-    with torch.no_grad():
-        test_preds = model(X_test)
-        test_mse = criterion(test_preds, y_test).item()
-        print(f"\nBaseline Test MSE on Unrotated Data: {test_mse:.4f}")
-        
-        # ------------------------------------------------------------
-        # The Vulnerability Test
-        # ------------------------------------------------------------
-        # Rotate the test set. An invariant target function will yield the exact same y_test.
-        # But this network's predictions will wildly change because it is sensitive to coordinate orientation.
-        R = get_random_rotation(d)
-        X_test_rotated = R.unsqueeze(0) @ X_test
-        
-        rotated_preds = model(X_test_rotated)
-        rotated_mse = criterion(rotated_preds, y_test).item()
-        
-        print(f"Test MSE on Rotated Data: {rotated_mse:.4f}")
-        
-        max_diff = torch.max(torch.abs(test_preds - rotated_preds)).item()
-        print(f"Max prediction variance caused strictly by rotation: {max_diff:.4f}")
+
+def save_losses(losses_tuple : tuple[list[float],...],
+                filename : str
+                ) -> None:
+    with open(filename, "wb") as f:
+        pickle.dump(losses_tuple, f)
+
+
+def load_losses(filename : str) -> None:
+    with open(filename, "rb") as f:
+        return pickle.load(f)
+
+
+if __name__ == "__main__":
+    # torch.manual_seed(42)
+    d = 2
+    num_epochs = 5
+    batch_size = 250
+    mlp_hidden_dims = (d**2,d**2)
+    polynomial = (0,1,1,)
+    
+    train_losses, test_losses, rotated_test_losses = train_test(
+        d=d,
+        num_epochs=num_epochs,
+        batch_size=batch_size,
+        mode=None,
+        mlp_hidden_dims=mlp_hidden_dims,
+        polynomial=polynomial
+    )
+
+    ctrain_losses, ctest_losses, crotated_test_losses = train_test(
+        d=d,
+        num_epochs=num_epochs,
+        batch_size=batch_size,
+        mode="canonicalize",
+        mlp_hidden_dims=mlp_hidden_dims,
+        polynomial=polynomial
+    )
+
+    qtrain_losses, qtest_losses, qrotated_test_losses = train_test(
+        d=d,
+        num_epochs=num_epochs,
+        batch_size=batch_size,
+        mode="quotient",
+        mlp_hidden_dims=mlp_hidden_dims,
+        polynomial=polynomial
+    )
+
+    # graph performance
+    epochs = range(1, num_epochs + 1)
+    fig, (ax1, ax2, ax3) = plt.subplots(1, 3, sharey=True, figsize=(18,4))
+    ax1.plot(epochs, train_losses, label="Train Loss", color="red")
+    ax1.plot(epochs, test_losses, label="Test Loss", color="blue")
+    ax1.plot(epochs, rotated_test_losses, label="Rotated Test Loss", color="yellow")
+    ax2.plot(epochs, ctrain_losses, label="Train Loss", color="red")
+    ax2.plot(epochs, ctest_losses, label="Test Loss", color="blue")
+    ax2.plot(epochs, crotated_test_losses, label="Rotated Test Loss", color="yellow")
+    ax3.plot(epochs, qtrain_losses, label="Train Loss", color="red")
+    ax3.plot(epochs, qtest_losses, label="Test Loss", color="blue")
+    ax3.plot(epochs, qrotated_test_losses, label="Rotated Test Loss", color="yellow")
+    
+    # subplot axis labels
+    ax1.set_title("Regular")
+    ax1.set_xlabel('Epochs')
+    ax2.set_title("Canonicalized")
+    ax2.set_xlabel('Epochs')
+    ax3.set_title("Quotiented")
+    ax3.set_xlabel('Epochs')
+    ax1.tick_params(axis='y', labelleft=True)
+    ax2.tick_params(axis='y', labelleft=True)
+    ax3.tick_params(axis='y', labelleft=True)
+    fig.supylabel('MSE Loss')
+    # figure title
+    fig.suptitle("Train vs Test vs Rotated Test Loss")
+    # enable legends and grid lines
+    ax1.legend()
+    ax2.legend()
+    ax3.legend()
+    ax1.grid(which='major', linestyle='-')
+    ax1.grid(which='minor', linestyle=':', alpha=0.5)
+    ax2.grid(which='major', linestyle='-')
+    ax2.grid(which='minor', linestyle=':', alpha=0.5)
+    ax3.grid(which='major', linestyle='-')
+    ax3.grid(which='minor', linestyle=':', alpha=0.5)
+
+    # grid formatting, shared since sharey=True
+    # ax1.set_yscale("log")
+    ax1.set_ylim(bottom=0)
+    ax1.yaxis.set_major_locator(MaxNLocator(nbins=20))
+    ax1.yaxis.set_minor_locator(AutoMinorLocator(5))
+
+    filename = f"piecewise-canonicalization/{d}-d_{num_epochs}-epochs_{polynomial}-poly"
+    save_losses(
+        losses_tuple=(train_losses, test_losses, rotated_test_losses,
+                      ctrain_losses, ctest_losses, crotated_test_losses,
+                      qtrain_losses, qtest_losses, qrotated_test_losses),
+        filename=filename+".pkl")
+    plt.savefig(filename+".png")
+    plt.show()
+    
